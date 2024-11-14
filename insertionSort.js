329bef29f8d8f11731902925d60049733f33bed5
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-11-14 16:52:03 +0530

    Update insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -1,15 +1,13 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    let currentValue = arr[i]; // Store the value of the current element
-    let j = i - 1; // Start with the element before the current one
-    // Shift elements to the right to make space for currentValue
+    let currentValue = arr[i]; 
+    let j = i - 1; 
     while (j >= 0 && arr[j] > currentValue) {
-      arr[j + 1] = arr[j]; // Shift the element one position to the right
-      j--; // Move to the next element on the left
+      arr[j + 1] = arr[j]; 
+      j--; 
     }
-    arr[j + 1] = currentValue; // Place currentValue in the correct position
+    arr[j + 1] = currentValue; 
   }
   return arr;
 }
-
-console.log(insertionSort([3, 2, 0])); // Output: [0, 2, 3]
+console.log(insertionSort([3, 2, 0]));
